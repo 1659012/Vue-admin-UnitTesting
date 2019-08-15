4d46e062d3568bd96a3673e05a342d2d172103a4
Hoan <lenguyenhanhoan@gmail.com>
2019-08-15 22:54:58 +0700

    add new test case

--- src/sum.test.js
+++ src/sum.test.js
@@ -4,6 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+test('adds 1 + (-3) to equal -2', () => {
+  expect(sum(1, -3)).toBe(-2);
+});
+
 test('adds undefined + 2 to equal undefined', () => {
   expect(sum(undefined, 2)).toBe(undefined);
 });
